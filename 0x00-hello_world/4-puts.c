0c60ea0f0a956189aef1afc895c96ecc56ea4e3f
Izoba <Sew308@yahoo.com>
2022-09-08 16:47:04 -0700

    Return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-* main -print the string in the put function
-*
-* Return: 
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multlingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
