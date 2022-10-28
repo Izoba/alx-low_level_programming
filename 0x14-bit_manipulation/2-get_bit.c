2ca36365b9c2ae154c9e1a63b3722c663db8f43c
Izoba <Sew308@yahoo.com>
2022-10-27 17:58:42 -0700

    BitManipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,31 +1,20 @@
 #include "main.h"
-#include <unistd.h>
-#include <limits.h>
+
 /**
- * print_binary - print an unsigned int in binary
- *
- * @n:
- *
- * Return: void
- */
-void print_binary(unsigned long int n)
+* get_bit - returns the value of a bit at an index in a decimal number
+* @n: number to search
+* @index: index of the bit
+*
+* Return: value of the bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int printbit = 1ul << 63;
-	char c = '0';
+	int bit_val;
 
-	while (!(printbit & n) && printbit != 0)
-		printbit = printbit >> 1;
+	if (index > 63)
+		return (-1);
 
-	if (printbit == 0)
-		write(1, &c, 1);
+	bit_val = (n >> index) & 1;
 
-	while (printbit)
-	{
-		if (printbit & n)
-			c = '1';
-		else
-			c = '0';
-		write(1, &c, 1);
-		printbit = printbit >> 1;
-	}
+	return (bit_val);
 }
